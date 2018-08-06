cef9792948c39bd5e5a4abc82be043e44c27968f
administrator <administrator@erpextend.localdomain>
2018-08-06 04:06:06 +0000

    Remove .js Auto Posting Date

--- workwise/time_keeping/doctype/official_business_application/official_business_application.js
+++ workwise/time_keeping/doctype/official_business_application/official_business_application.js
@@ -3,9 +3,9 @@ cur_frm.add_fetch('employee','company','company');
 
 frappe.ui.form.on('Official Business Application', {
 	onload: function(frm) {
-		if (!frm.doc.posting_date) {
-			frm.set_value("posting_date", get_today());
-		}
+		//if (!frm.doc.posting_date) {
+		//	frm.set_value("posting_date", get_today());
+		//}
 	},
 
 	refresh: function(frm) {
