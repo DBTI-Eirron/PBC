24de06b4c452fbdcea7c7cf507e9b1521b0842f7
administrator <administrator@erpextend.localdomain>
2018-08-29 04:00:03 +0000

    Joblevel Filter

--- workwise/employee_201/emp_filters_utils.py
+++ workwise/employee_201/emp_filters_utils.py
@@ -23,8 +23,8 @@ def empget_employees(filter_type, filter_value, company):
 			"filter_value": filter_value,
 		}, as_dict=True)
 
-	elif filter_type == 'Location':
-		employees = frappe.db.sql("""SELECT `name`, `full_name` FROM tabEmployee WHERE company = %(company)s  AND location = %(filter_value)s ORDER BY last_name, first_name""",{ 
+	elif filter_type == 'Job Level':
+		employees = frappe.db.sql("""SELECT `name`, `full_name` FROM tabEmployee WHERE company = %(company)s  AND job_level = %(filter_value)s ORDER BY last_name, first_name""",{ 
 			"company": company,
 			"filter_value": filter_value,
 		}, as_dict=True)
