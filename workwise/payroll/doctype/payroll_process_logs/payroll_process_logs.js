90f2cc2d07eb8898dabf86a3d2468e1ab4abd49c
administrator <administrator@erpextend.localdomain>
2018-09-04 09:57:12 +0000

    Process logs on making payslips

---
+++ workwise/payroll/doctype/payroll_process_logs/payroll_process_logs.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2018, HDI Systech and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Payroll Process Logs', {
+	refresh: function(frm) {
+
+	}
+});
