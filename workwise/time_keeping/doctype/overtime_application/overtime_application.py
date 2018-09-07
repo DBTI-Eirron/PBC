ae6427b00adc35a13be3f0987b4eac5232eba771
administrator <administrator@erpextend.localdomain>
2018-09-07 07:43:13 +0000

    Overtime Application - Added new validation

--- workwise/time_keeping/doctype/overtime_application/overtime_application.py
+++ workwise/time_keeping/doctype/overtime_application/overtime_application.py
@@ -125,3 +125,10 @@ class OvertimeApplication(Document):
 		for d in application:
 			if d.name:
 				frappe.throw(_("Application already exists, {0}.").format(d.name))
+
+
+		ot_application = frappe.db.sql(""" SELECT `name` FROM `tabOvertime Application` WHERE `docstatus` = 1 AND `employee` = %s AND `from_date` = %s AND `to_date` = %s AND (%s BETWEEN from_time AND to_time) AND (%s BETWEEN from_time AND to_time) """,(self.employee, self.from_date, self.to_date, self.to_time, self.from_time), as_dict=True)
+
+		for e in ot_application:
+			if e.name:
+				frappe.throw(_("Application already exists, {0}.").format(e.name))
