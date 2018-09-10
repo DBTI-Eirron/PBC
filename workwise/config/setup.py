0883eaeb0882dd4525d6dfe057ad55d6a6ee1b9c
administrator <administrator@erpextend.localdomain>
2018-09-10 06:53:05 +0000

    New Feature Important: Jasper Forms

---
+++ workwise/config/setup.py
@@ -0,0 +1,21 @@
+from __future__ import unicode_literals
+from frappe import _
+from frappe.desk.moduleview import add_setup_section
+
+def get_data():
+	return [
+		{
+			"label": _("Jasper Settings"),
+			"icon": "fa fa-star",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Jasper Forms",
+				},
+				{
+					"type": "doctype",
+					"name": "Jasper Settings",
+				},
+			]
+		},
+	]
---
+++ workwise/setup/doctype/jasper_form/jasper_form.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2018, HDI Systech and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+
+class JasperForm(Document):
+	pass
+
+@frappe.whitelist()
+def get_forms(doctype_name):
+	forms = frappe.db.sql(""" SELECT * FROM `tabJasper Form` WHERE linked_doctype = %s """, doctype_name, as_dict=True)
+	for d in forms:
+		d['form_ip'] = frappe.db.get_single_value('Jasper Settings', 'jasper_ip')
+		d['form_port'] = frappe.db.get_single_value('Jasper Settings', 'jasper_port')
+		d['form_user'] = frappe.db.get_single_value('Jasper Settings', 'jasper_user')
+		d['form_pass'] = frappe.db.get_single_value('Jasper Settings', 'jasper_pass')
+
+	return forms
