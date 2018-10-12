0e9fa527e203c0110d27b1400c70fa7060be4c87
administrator <administrator@erpextend.localdomain>
2018-10-12 03:40:35 +0000

    BugFix Attendance Utils

--- workwise/time_keeping/application_utils.py
+++ workwise/time_keeping/application_utils.py
@@ -5,13 +5,14 @@ from frappe.utils import cstr, cint, flt, nowdate, add_days, getdate, fmt_money
 from frappe import _
 
 def grant_head_subordinate_access(self):
-	reject_head_access = frappe.db.get_single_value('System Settings', 'head_not_allowed_for_subordinate')
-	if reject_head_access == 1:
-		subordinate = frappe.db.sql(""" SELECT `for_value` FROM `tabUser Permission` WHERE `allow` = "Employee" AND `for_value` = %s AND `user` = %s """, ( self.employee, frappe.session.user ), as_dict=True)
-		if subordinate:
-			user_id = frappe.db.sql("""SELECT user_id FROM `tabEmployee` WHERE `name` = %s LIMIT 1""",( self.employee ), as_dict=1)
-			if user_id[0].user_id != frappe.session.user:
-				frappe.throw(_("You Cannot Create Application In Behalf Of Your Subordinate"))
+	if self.is_new():
+		reject_head_access = frappe.db.get_single_value('System Settings', 'head_not_allowed_for_subordinate')
+		if reject_head_access == 1:
+			subordinate = frappe.db.sql(""" SELECT `for_value` FROM `tabUser Permission` WHERE `allow` = "Employee" AND `for_value` = %s AND `user` = %s """, ( self.employee, frappe.session.user ), as_dict=True)
+			if subordinate:
+				user_id = frappe.db.sql("""SELECT user_id FROM `tabEmployee` WHERE `name` = %s LIMIT 1""",( self.employee ), as_dict=1)
+				if user_id[0].user_id != frappe.session.user:
+					frappe.throw(_("You Cannot Create Application In Behalf Of Your Subordinate"))
 
 def get_approver_and_date(self):
 	self.approved_by = frappe.session.user
