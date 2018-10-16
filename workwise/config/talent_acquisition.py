165af2e62e1fc2043857b0b0cc55d3720df5648f
administrator <administrator@erpextend.localdomain>
2018-10-16 04:29:03 +0000

    Reaarange Talent Acquisition Modules

--- workwise/config/talent_acquisition.py
+++ workwise/config/talent_acquisition.py
@@ -7,6 +7,11 @@ def get_data():
 			"label": _("Recruitment"),
 			"items": [
 				{
+					"type": "page",
+					"name": "applicant_monitoring",
+					"label": _("Applicant Monitoring"),
+				},
+				{
 					"type": "doctype",
 					"name": "Job Applicant",
 					"description": _("Job Applicant"),
@@ -18,34 +23,29 @@ def get_data():
 				},				
 				{
 					"type": "doctype",
-					"name": "Job Opening",
-					"description": _("Job Opening"),
+					"name": "Interview and Background",
+					"label": _("Interview and Background"),
 				},
 				{
 					"type": "doctype",
 					"name": "Offer Letter",
 					"description": _("Offer Letter"),
 				},
+			]
+		},
+		{
+			"label": _("Tools"),
+			"items": [
 				{
 					"type": "doctype",
 					"name": "Personnel Requisition",
 					"description": _("Personnel Requisition"),
 				},
 				{
-					"type": "page",
-					"name": "applicant_monitoring",
-					"label": _("Applicant Monitoring"),
-				},	
-				{
-					"type": "page",
-					"name": "interview_and_background",
-					"label": _("Interview and Background"),
+					"type": "doctype",
+					"name": "Job Opening",
+					"description": _("Job Opening"),
 				},
-			]
-		},
-		{
-			"label": _("Reports"),
-			"items": [
 				{
 					"type": "report",
 					"name": "Plantilla Report",
