99bdeebf4066ee04dd8707f3d1586b03d6339b80
administrator <administrator@erpextend.localdomain>
2018-10-17 03:07:03 +0000

    Notifications Adjustments for , Blanket ,Work Suspension CTO

--- workwise/notifications/notifications.py
+++ workwise/notifications/notifications.py
@@ -13,13 +13,17 @@ def get_notification_config():
 			"Leave Application": {"workflow_state": "Pending"},
 			"Overtime Application": {"workflow_state": "Pending"},
 			"Official Business Application": {"workflow_state": "Pending"},
+			"Change Schedule Application": {"workflow_state": "Pending"},
 			"Undertime Application": {"workflow_state": "Pending"},
 			"Excuse Tardiness Application": {"workflow_state": "Pending"},
 			"DTR Problem Application": {"workflow_state": "Pending"},
-			"Change Schedule Application": {"workflow_state": "Pending"},
+			"Compensatory Time Off": {"workflow_state": "Pending"},
+			"Blanket": {"workflow_state": "Pending"},
+			"Work Suspension": {"workflow_state": "Pending"},
 			"Loan Application": {"workflow_state": "Pending"},
 			"Appraisal": {"appraisal_type": "360-Degree"},
 			"Job Applicant": {"status": "Open"},
+			
 		}
 	}
 
