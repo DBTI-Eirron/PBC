b92753aa2d4010f171ee491afe981f135434aa7d
administrator <administrator@erpextend.localdomain>
2018-10-23 01:17:42 +0000

    phic premium contribution report relink jasper reports

--- workwise/payroll/report/philhealth_premium_contribution/philhealth_premium_contribution.js
+++ workwise/payroll/report/philhealth_premium_contribution/philhealth_premium_contribution.js
@@ -23,26 +23,33 @@ frappe.query_reports["PhilHealth Premium Contribution"] = {
 
 	onload: function(report) {
 		var host_link = window.location.hostname+":8080";
-		report.page.add_inner_button(__("Print"), function() {
+		report.page.add_inner_button(__("Print PHIC Certificate"), function() {
 			var from_date = frappe.query_report_filters_by_name.from_date.get_value();
 			var to_date = frappe.query_report_filters_by_name.to_date.get_value();
 			var company = frappe.query_report_filters_by_name.company.get_value();
-			window.open("http://"+host_link+"/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2FReports&reportUnit=%2FReports%2FPHIC_remittance&standAlone=true&j_username=jasperadmin&j_password=jasperadmin&output=pdf&company="+company+"&from_date="+from_date+"&to_date="+to_date+"");
-		});
+			window.open("http://"+host_link+"/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2FReports&reportUnit=%2FReports%2Fphic_certificate&standAlone=true&j_username=jasperadmin&j_password=jasperadmin&output=pdf&company="+company+"&from_date="+from_date+"&to_date="+to_date+"");
+		});		
+		//var host_link = window.location.hostname+":8080";
+		//report.page.add_inner_button(__("Print PHIC Remittance Txt Form"), function() {
+		//	var from_date = frappe.query_report_filters_by_name.from_date.get_value();
+		//	var to_date = frappe.query_report_filters_by_name.to_date.get_value();
+		//	var company = frappe.query_report_filters_by_name.company.get_value();
+		//	window.open("http://"+host_link+"/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2FReports&reportUnit=%2FReports%2F_remittance&standAlone=true&j_username=jasperadmin&j_password=jasperadmin&output=pdf&company="+company+"&from_date="+from_date+"&to_date="+to_date+"");
+		//});
 		var host_link = window.location.hostname+":8080";
-		report.page.add_inner_button(__("PCP Form"), function() {
+		report.page.add_inner_button(__("Print RF-1"), function() {
 			var from_date = frappe.query_report_filters_by_name.from_date.get_value();
 			var to_date = frappe.query_report_filters_by_name.to_date.get_value();
 			var company = frappe.query_report_filters_by_name.company.get_value();
-			window.open("http://"+host_link+"/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2FReports&reportUnit=%2FReports%2FPCP_Form&standAlone=true&j_username=jasperadmin&j_password=jasperadmin&output=pdf&company="+company+"&from_date="+from_date+"&to_date="+to_date+"");
+			window.open("http://"+host_link+"/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2FReports&reportUnit=%2FReports%2Frf1_form&standAlone=true&j_username=jasperadmin&j_password=jasperadmin&output=pdf&company="+company+"&from_date="+from_date+"&to_date="+to_date+"");
 		});
-		var host_link = window.location.hostname+":8080";
-		report.page.add_inner_button(__("PCPQ Form"), function() {
-			var from_date = frappe.query_report_filters_by_name.from_date.get_value();
-			var to_date = frappe.query_report_filters_by_name.to_date.get_value();
-			var company = frappe.query_report_filters_by_name.company.get_value();
-			window.open("http://"+host_link+"/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2FReports&reportUnit=%2FReports%2FPCP_Q_Form&standAlone=true&j_username=jasperadmin&j_password=jasperadmin&output=pdf&company="+company+"&from_date="+from_date+"&to_date="+to_date+"");
-		});
+		//var host_link = window.location.hostname+":8080";
+		//report.page.add_inner_button(__("Print RF-1 Quarterly"), function() {
+		//	var from_date = frappe.query_report_filters_by_name.from_date.get_value();
+		//	var to_date = frappe.query_report_filters_by_name.to_date.get_value();
+		//	var company = frappe.query_report_filters_by_name.company.get_value();
+		//	window.open("http://"+host_link+"/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2FReports&reportUnit=%2FReports%2FPCP_Q_Form&standAlone=true&j_username=jasperadmin&j_password=jasperadmin&output=pdf&company="+company+"&from_date="+from_date+"&to_date="+to_date+"");
+		//});
 	},/*,
 	"formatter": function(row, cell, value, columnDef, dataContext, default_formatter) {
 		if (columnDef.df.fieldname=="account") {
