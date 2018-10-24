fc1e725864e9f6fc92c996d28d632818c4959cac
administrator <administrator@erpextend.localdomain>
2018-10-24 05:53:24 +0000

    PHIC contrib report added quarterly

--- workwise/payroll/report/philhealth_premium_contribution/philhealth_premium_contribution.js
+++ workwise/payroll/report/philhealth_premium_contribution/philhealth_premium_contribution.js
@@ -43,13 +43,13 @@ frappe.query_reports["PhilHealth Premium Contribution"] = {
 			var company = frappe.query_report_filters_by_name.company.get_value();
 			window.open("http://"+host_link+"/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2FReports&reportUnit=%2FReports%2Frf1_form&standAlone=true&j_username=jasperadmin&j_password=jasperadmin&output=pdf&company="+company+"&from_date="+from_date+"&to_date="+to_date+"");
 		});
-		//var host_link = window.location.hostname+":8080";
-		//report.page.add_inner_button(__("Print RF-1 Quarterly"), function() {
-		//	var from_date = frappe.query_report_filters_by_name.from_date.get_value();
-		//	var to_date = frappe.query_report_filters_by_name.to_date.get_value();
-		//	var company = frappe.query_report_filters_by_name.company.get_value();
-		//	window.open("http://"+host_link+"/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2FReports&reportUnit=%2FReports%2FPCP_Q_Form&standAlone=true&j_username=jasperadmin&j_password=jasperadmin&output=pdf&company="+company+"&from_date="+from_date+"&to_date="+to_date+"");
-		//});
+		var host_link = window.location.hostname+":8080";
+		report.page.add_inner_button(__("Print RF-1 Quarterly"), function() {
+			var from_date = frappe.query_report_filters_by_name.from_date.get_value();
+			var to_date = frappe.query_report_filters_by_name.to_date.get_value();
+			var company = frappe.query_report_filters_by_name.company.get_value();
+			window.open("http://"+host_link+"/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2FReports&reportUnit=%2FReports%2Frf1_quarterly_form&standAlone=true&j_username=jasperadmin&j_password=jasperadmin&output=pdf&company="+company+"&from_date="+from_date+"&to_date="+to_date+"");
+		});
 	},/*,
 	"formatter": function(row, cell, value, columnDef, dataContext, default_formatter) {
 		if (columnDef.df.fieldname=="account") {
