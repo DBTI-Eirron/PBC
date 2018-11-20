1d5a510776450006c28e3459530c5a01c46a9e13
administrator <administrator@erpextend.localdomain>
2018-11-20 06:36:08 +0000

    Deleted unneeded strategic modules files

--- workwise/performance_management/doctype/employee_appraisal/employee_appraisal.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2017, HDI Systech and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Employee Appraisal', {
-	refresh: function(frm) {
-
-	}
-});
