6039f559561ed12aa38a526080713044f598c021
administrator <administrator@erpextend.localdomain>
2018-11-23 03:44:47 +0000

    Talent Acquisition Revisions

--- workwise/talent_acquisition/doctype/job_applicant/job_applicant.js
+++ workwise/talent_acquisition/doctype/job_applicant/job_applicant.js
@@ -16,5 +16,19 @@ frappe.ui.form.on('Job Applicant', {
 				cur_frm.page.set_inner_btn_group_as_primary(__("Make"));
 			}
 		}
+		frappe.call({
+			method: "workwise.setup.doctype.jasper_form.jasper_form.get_forms",
+			args:{
+				doctype_name: "Job Applicant"
+			},
+			callback: function(r) {
+				r.message.forEach(function(item) {
+					frm.add_custom_button(__(item.form_label),
+					function() {
+						window.open("http://"+ item.form_ip +":"+ item.form_port +"/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2F"+ item.form_folder +"&reportUnit=%2FReports%2F"+ item.form_name +"&standAlone=true&j_username=jasperadmin&j_password=jasperadmin&output=pdf&filter1="+frm.doc.name+"");
+					});
+				});
+			}
+		});
 	},
 });
