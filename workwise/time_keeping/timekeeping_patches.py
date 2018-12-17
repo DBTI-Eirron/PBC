d8c6745bd41c450ee8382e4339064a09a06ce72b
administrator <administrator@erpextend.localdomain>
2018-12-17 07:24:04 +0000

    Added Company Deletion Script

--- workwise/time_keeping/timekeeping_patches.py
+++ workwise/time_keeping/timekeeping_patches.py
@@ -21,3 +21,64 @@ def update_old_change_schedule_application():
 def oba_update_table():
 	frappe.db.sql("""UPDATE `tabOfficial Business Application Table` SET travel_time = travel_time, hrs = hrs, target_date = target_date, `date` = `target_date`, from_time = from_time, to_time = to_time, is_holiday = is_holiday, is_excluded = is_excluded, is_previous = 0 WHERE `date` IS NULL AND docstatus != 2 """)
 	frappe.db.commit()
+
+def qetquery_delete_company_records():
+	query_list = []
+	format2 = ""
+	format1 = ""
+	company = ["Direc Business Solutions Inc.", "Direc Business Technologies Inc."]
+	doctype_list = frappe.db.sql(""" SELECT DISTINCT `name` FROM `tabDocType` WHERE issingle = 0 AND istable = 0 AND `name` IN (SELECT DISTINCT parent FROM `tabDocField` WHERE fieldname = "company") """, as_dict=1)
+
+	if doctype_list:
+		for doc in doctype_list:
+			table = "`tab"+doc.name+"`"
+			table = str(table)
+
+			for com in company:
+				to_del = frappe.db.sql(""" SELECT DISTINCT `name` FROM """+table+""" WHERE company = %s """,(com), as_dict=1)
+				if to_del:
+					has_childtable = frappe.db.sql(""" SELECT DISTINCT `options` FROM `tabDocField` WHERE fieldtype = "Table" AND `parent` = %s LIMIT 1 """,(doc.name), as_dict=1)
+					if has_childtable:
+						for to in to_del:
+							child_table = "`tab"+has_childtable[0].options+"`"
+							child_table = str(child_table)
+							format2 = "'"+com+"'"
+				
+						to_app = str(" DELETE T1, T2 FROM "+table+" T1 JOIN "+child_table+" T2 ON T1.`name` = T2.parent WHERE T1.company = "+format2+";")
+						query_list.append(to_app)
+					
+					format2 = "'"+com+"'"
+					to_append = str(" DELETE FROM "+table+" WHERE company ="+format2+";")
+					query_list.append(to_append)
+
+					to_appnd = str(" DELETE FROM `tabCompany` WHERE `name` ="+format2+";")
+					query_list.append(to_appnd)
+	
+	frappe.throw(_(''.join(query_list)))
+
+def delete_company_records():
+	company = ["Direc Business Solutions Inc.", "Direc Business Technologies Inc."]
+	doctype_list = frappe.db.sql(""" SELECT DISTINCT `name` FROM `tabDocType` WHERE issingle = 0 AND istable = 0 AND `name` IN (SELECT DISTINCT parent FROM `tabDocField` WHERE fieldname = "company") """, as_dict=1)
+
+	if doctype_list:
+		for doc in doctype_list:
+			table = "`tab"+doc.name+"`"
+			table = str(table)
+
+			for com in company:
+				to_del = frappe.db.sql(""" SELECT DISTINCT `name` FROM """+table+""" WHERE company = %s """,(com), as_dict=1)
+				if to_del:
+					has_childtable = frappe.db.sql(""" SELECT DISTINCT `options` FROM `tabDocField` WHERE fieldtype = "Table" AND `parent` = %s LIMIT 1 """,(doc.name), as_dict=1)
+					if has_childtable:
+						for to in to_del:
+							child_table = "`tab"+has_childtable[0].options+"`"
+							child_table = str(child_table)
+						
+						frappe.db.sql("""DELETE T1, T2 FROM """+table+""" T1 JOIN """+child_table+""" T2 ON T1.`name` = T2.parent WHERE T1.company = %s """,(com))
+						frappe.db.commit()
+					
+					frappe.db.sql("""DELETE FROM """+table+""" WHERE company = %s """,(com))
+					frappe.db.commit()
+
+				frappe.db.sql("""DELETE FROM `tabCompany` WHERE `name` = %s """,(com))
+				frappe.db.commit()
