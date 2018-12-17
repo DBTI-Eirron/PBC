1ed0d6e47327fef2699c6567ba17d230540240ca
administrator <administrator@erpextend.localdomain>
2018-12-17 07:05:50 +0000

    EXT Application Timelogs Validation

--- workwise/time_keeping/doctype/excuse_tardiness_application/excuse_tardiness_application.py
+++ workwise/time_keeping/doctype/excuse_tardiness_application/excuse_tardiness_application.py
@@ -15,6 +15,9 @@ class ExcuseTardinessApplication(Document):
 		change_owner(self)
 
 	def on_submit(self):
+		time_in, time_out = self.get_timelogs()
+		if not time_in and not time_out:
+			frappe.throw(("No timelogs for employee"))
 		validate_approve_own_application(self)
 		get_approver_and_date(self)
 
@@ -22,11 +25,16 @@ class ExcuseTardinessApplication(Document):
 		validate_reject_cancel_own_application(self)
 
 	def load_timecard(self):
+		time_in, time_out = self.get_timelogs()
+		if time_in:
+			self.to_time = time_in[0].time
+		if time_out:
+			self.from_time = time_out[0].time
+
+	def get_timelogs(self):
 		bio_id = frappe.get_value("Employee", self.employee, "biometrics_id")
 		if bio_id:
-			time_in = frappe.db.sql(""" SELECT `time` FROM `tabTime Card` WHERE `biometrics_id` = %s AND `card_type` = 1 AND `is_disabled` = 0 AND `date` = %s LIMIT 1 """, (bio_id, self.date), as_dict=True)
-			if time_in:
-				self.to_time = time_in[0].time
 			time_out = frappe.db.sql(""" SELECT `time` FROM `tabTime Card` WHERE `biometrics_id` = %s AND `card_type` = 0 AND `is_disabled` = 0 AND `date` = %s LIMIT 1 """, (bio_id, self.date), as_dict=True)
-			if time_out:
-				self.from_time = time_in[0].time
+			time_in = frappe.db.sql(""" SELECT `time` FROM `tabTime Card` WHERE `biometrics_id` = %s AND `card_type` = 1 AND `is_disabled` = 0 AND `date` = %s LIMIT 1 """, (bio_id, self.date), as_dict=True)
+
+			return time_in, time_out
