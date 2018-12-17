b80b03ae502d5d659669bac6c414c500913cbac8
administrator <administrator@erpextend.localdomain>
2018-12-17 07:30:59 +0000

    Bumped to Version 1.0.53

--- workwise/hooks.py
+++ workwise/hooks.py
@@ -10,7 +10,7 @@ app_icon = "octicon octicon-book"
 app_color = "#589494"
 app_email = "krefin.fagara@gmail.com"
 app_license = "GNU General Public License"
-app_version = "1.0.32"
+app_version = "1.0.53"
 
 # Includes in <head>
 # ------------------
