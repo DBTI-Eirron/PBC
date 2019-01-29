4a2085199d1700f3c1004f5710b6e1da4d7d75b8
administrator <administrator@erpextend.localdomain>
2019-01-29 06:07:44 +0000

    Automatic appearance for Employee name and Postion title upon selection of employee on Doctype Exit Interview

--- workwise/employee_201/doctype/exit_interview/exit_interview.js
+++ workwise/employee_201/doctype/exit_interview/exit_interview.js
@@ -1,5 +1,7 @@
 // Copyright (c) 2017, HDI Systech and contributors
 // For license information, please see license.txt
+cur_frm.add_fetch('employee','full_name','full_name');
+cur_frm.add_fetch('employee','position_title','position_title');
 
 frappe.provide("workwise.exit_interview");
 
