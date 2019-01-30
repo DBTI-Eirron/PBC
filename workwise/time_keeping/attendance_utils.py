9c44342249be3b031c5fe4208693a1099990a740
administrator <administrator@erpextend.localdomain>
2019-01-30 09:04:55 +0000

    Bugfix UT on 2ndHalf Leave OB 1st half

--- workwise/time_keeping/attendance_utils.py
+++ workwise/time_keeping/attendance_utils.py
@@ -403,13 +403,15 @@ def get_undertime(entry):
 			else: #if no card in check for OB
 				if entry.get('ob_status') == 1:
 					if entry.get('ob_out') < entry.get('break_end'): #if OB is in first half
-						entry['undertime'] += abs((entry.get('break_end') - entry.get('time_out')).total_seconds())
-						if entry.get('ob_out') < entry.get('break_start'): #Add undertime Beyond Break Time
-							entry['undertime'] += abs((entry.get('ob_out') - entry.get('break_start')).total_seconds())
+						if not entry.get('lv_status') == 3:
+							entry['undertime'] += abs((entry.get('break_end') - entry.get('time_out')).total_seconds())
+							if entry.get('ob_out') < entry.get('break_end'): #Add undertime Beyond Break Time
+								entry['undertime'] -= abs((entry.get('ob_out') - entry.get('break_start')).total_seconds())
 					else:
 						if entry.get('ob_out') < entry.get('time_out'): #if OB is wholeday
 							entry['undertime'] += abs((entry.get('ob_out') - entry.get('time_out')).total_seconds())
 		
+
 		if entry.get('ut_interval'):
 			entry['undertime'] = (entry.get('ut_interval') * 60) * int( entry.get('undertime') / (entry.get('ut_interval') * 60))
 
