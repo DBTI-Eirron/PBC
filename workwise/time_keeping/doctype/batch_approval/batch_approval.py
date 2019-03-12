d115e10859179893a9af173715f33595124166e1
administrator <administrator@erpextend.localdomain>
2019-03-12 09:28:33 +0000

    Bugfix for Batch Approval

--- workwise/time_keeping/doctype/batch_approval/batch_approval.py
+++ workwise/time_keeping/doctype/batch_approval/batch_approval.py
@@ -3,9 +3,10 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
-from frappe import _
-from frappe.utils import getdate, nowdate, flt
+import frappe, datetime
+from datetime import time, datetime, timedelta
+from frappe.utils import cstr, cint, flt, nowdate, add_days, getdate, fmt_money, now_datetime
+from frappe import _, msgprint
 from frappe.model.document import Document
 
 class BatchApproval(Document):
@@ -99,14 +100,40 @@ class BatchApproval(Document):
 		table = "`tab"+self.application_type+"`"
 		for b in self.get("batch_table"):
 			if b.action == "Approved":
-				application = frappe.get_doc(self.application_type, b.application)
-				application.update({
-					"workflow_state": "Approved",
-					"approved_by": frappe.session.user,
-					"approved_on": nowdate(),
-				})
-				application.save()
-				application.submit()
+				enable_employee_approvers = frappe.db.get_single_value('Timekeeping Settings', 'enable_employee_approvers')
+				if enable_employee_approvers > 0:
+					approval_history = ""
+					approver_level = frappe.db.sql(""" SELECT IFNULL(MAX(EA.`level`), 0) as `level` FROM `tabEmployee Approvers` EA JOIN `tabEmployee` TE ON EA.`approver` = TE.`name` WHERE EA.parenttype = "Employee" AND (EA.application = %s OR EA.application = "All") AND EA.parent = %s AND TE.user_id = %s """,(self.application_type, b.employee, frappe.session.user), as_dict=True)
+					highest_level = frappe.db.sql(""" SELECT IFNULL(MAX(`level`), 0) as level FROM `tabEmployee Approvers` WHERE parenttype = "Employee" AND (`application` = %s OR `application` = "All") AND parent = %s """,(self.application_type, b.employee), as_dict=True)	
+					if approver_level[0].level == highest_level[0].level:
+						if approval_history is not None:
+							approval_history = frappe.db.get_value(self.application_type, b.application, "approval_history")
+						else:
+							approval_history = ""
+						approval_history = str(approval_history)+"Level "+str(highest_level[0].level)+": "+str(frappe.session.user)+" approved on "+str(now_datetime().strftime('%Y-%m-%d %H:%M:%S'))+"\n"
+						suc = frappe.db.sql("""UPDATE """+table+""" SET approval_history = %s, last_approval_level = %s, workflow_state = "Approved", approved_by = %s, approved_on = %s WHERE `name` = %s """, (approval_history, highest_level[0].level, frappe.session.user, nowdate(), b.application))
+						frappe.db.commit()
+						if suc:
+							frappe.msgprint(_("<b>{0}: {1}</b><hr> Approval Successful").format(self.application_type, b.application))
+					else:
+						if approval_history is not None:
+							approval_history = frappe.db.get_value(self.application_type, b.application, "approval_history")
+						else:
+							approval_history = ""
+						approval_history = str(approval_history)+"Level "+str(approver_level[0].level)+": "+str(frappe.session.user)+" approved on "+str(now_datetime().strftime('%Y-%m-%d %H:%M:%S'))+"\n"
+						suc = frappe.db.sql("""UPDATE """+table+""" SET approval_history = %s, last_approval_level = %s, workflow_state = "Approval in Progress" WHERE `name` = %s """, (approval_history, approver_level[0].level, b.application))
+						frappe.db.commit()
+						if suc:
+							frappe.msgprint(_("<b>{0}: {1}</b><hr> Approval Successful").format(self.application_type, b.application))
+				else:
+					application = frappe.get_doc(self.application_type, b.application)
+					application.update({
+						"workflow_state": "Approved",
+						"approved_by": frappe.session.user,
+						"approved_on": nowdate(),
+					})
+					application.save()
+					application.submit()
 			if b.action == "Rejected":
 				frappe.db.sql("""UPDATE """+table+""" SET docstatus = 2, workflow_state = "Rejected" WHERE `name` = %s """, (b.application))
 				frappe.db.commit()
@@ -148,27 +175,47 @@ class BatchApproval(Document):
 				filter_date = "AP.`date` or AP.use_date"
 
 		if not "Administrator" in frappe.get_roles(cur_user):
-			record = frappe.db.sql(""" SELECT DISTINCT AP.`name`, AP.`posting_date`, AP.`employee`, TE.`full_name`"""+additional_fields+""" 
-				FROM """+table+""" AP JOIN `tabEmployee` TE ON AP.`employee` = TE.`name` 
-				WHERE AP.`workflow_state` = "Pending"
-				AND ("""+filter_date+""" BETWEEN %(from_date)s AND %(to_date)s) 
-				AND TE.`name` IN (SELECT `for_value` FROM `tabUser Permission` WHERE `allow` = "Employee" AND `user` = %(cur_user)s)
-				{conditions} 
-				AND TE.company = %(company)s """.format(conditions=self.sql_select_filters()),{ 
-					"company": self.company,
-					"employee": self.employee,
-					"from_date": getdate(self.from_date),
-					"to_date": getdate(self.to_date),
-					"cur_user": cur_user,
+			enable_employee_approvers = frappe.db.get_single_value('Timekeeping Settings', 'enable_employee_approvers')
+			if enable_employee_approvers > 0:
+				record = frappe.db.sql(""" SELECT DISTINCT AP.`name`, AP.`posting_date`, AP.`employee`, TE.`full_name` """+additional_fields+""" 
+						FROM """+table+""" AP JOIN `tabEmployee` TE ON AP.`employee` = TE.`name` 
+						WHERE ( AP.`workflow_state` = "Pending" OR AP.`workflow_state` = "Approval in Progress" ) 
+						AND ( """+filter_date+""" BETWEEN %(from_date)s AND %(to_date)s ) 
+						AND TE.`name` IN ( SELECT `for_value` FROM `tabUser Permission` WHERE `allow` = "Employee" AND `user` = %(cur_user)s ) {conditions} 
+						AND TE.company = %(company)s
+						AND AP.`employee` IN (SELECT DISTINCT AE.`parent` FROM `tabEmployee` ET INNER JOIN `tabEmployee Approvers` AE ON ET.`name`=AE.`approver` WHERE ET.user_id = %(cur_user)s AND (AE.`application`=%(application)s OR AE.`application`="All") AND AE.`level` = AP.`last_approval_level`+1)
+					""".format(conditions=self.sql_select_filters()),{ 
+						"company": self.company,
+						"employee": self.employee,
+						"from_date": getdate(self.from_date),
+						"to_date": getdate(self.to_date),
+						"application": self.application_type,
+						"cur_user": cur_user,
 				}, as_dict=True)
+			else:
+				record = frappe.db.sql(""" SELECT DISTINCT AP.`name`, AP.`posting_date`, AP.`employee`, TE.`full_name`"""+additional_fields+""" 
+						FROM """+table+""" AP JOIN `tabEmployee` TE ON AP.`employee` = TE.`name` 
+						WHERE (AP.`workflow_state` = "Pending" OR AP.`workflow_state` = "Approval in Progress")
+						AND ("""+filter_date+""" BETWEEN %(from_date)s AND %(to_date)s) 
+						AND TE.`name` IN (SELECT `for_value` FROM `tabUser Permission` WHERE `allow` = "Employee" AND `user` = %(cur_user)s)
+						{conditions} 
+						AND TE.company = %(company)s 
+					""".format(conditions=self.sql_select_filters()),{ 
+						"company": self.company,
+						"employee": self.employee,
+						"from_date": getdate(self.from_date),
+						"to_date": getdate(self.to_date),
+						"cur_user": cur_user,
+					}, as_dict=True)
 		else:
 			record = frappe.db.sql("""SELECT AP.`name`, AP.`posting_date`, AP.`employee`, TE.`full_name`"""+additional_fields+""" 
-				FROM """+table+""" AP JOIN `tabEmployee` TE 
-				WHERE AP.`employee` = TE.`name` 
-				AND AP.`workflow_state` = "Pending"
-				AND ("""+filter_date+""" BETWEEN %(from_date)s AND %(to_date)s) 
-				{conditions}
-				AND TE.company = %(company)s """.format(conditions=self.sql_select_filters()),{ 
+					FROM """+table+""" AP JOIN `tabEmployee` TE 
+					WHERE AP.`employee` = TE.`name` 
+					AND (AP.`workflow_state` = "Pending" OR AP.`workflow_state` = "Approval in Progress")
+					AND ("""+filter_date+""" BETWEEN %(from_date)s AND %(to_date)s) 
+					{conditions}
+					AND TE.company = %(company)s 
+				""".format(conditions=self.sql_select_filters()),{ 
 					"company": self.company,
 					"employee": self.employee,
 					"from_date": getdate(self.from_date),
@@ -176,3 +223,6 @@ class BatchApproval(Document):
 				}, as_dict=True)
 
 		return record
+
+
+
