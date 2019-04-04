749f486872130410067eb702694f4799bbf4677f
Kinzunite <kinzunite@gmail.com>
2019-04-04 21:12:44 +0800

    Timekeeping Task Fix

--- workwise/time_keeping/timekeeping_task.py
+++ workwise/time_keeping/timekeeping_task.py
@@ -7,12 +7,23 @@ def get_scheduler():
 	pass
 
 def leave_balance_monthly():
-	balance_scheds = frappe.db.sql(""" SELECT LB.employee, LB.employee_name, LBS.leave_type, LBS.trigger_on, LBS.credit 
-		FROM `tabLeave Balance` LB 
+	balance_scheds = frappe.db.sql(""" SELECT LB.employee, LB.employee_name, LBS.leave_type, LBS.trigger_on, LBS.credits 
+		FROM `tabLeave Balance Setup` LB 
 		INNER JOIN `tabLeave Balance Schedule` LBS ON LBS.`parent` = LB.`name` WHERE LBS.trigger_on = 'Every Month' """, as_dict=1)
 
+	now_date = nowdate()
+	year_end =getdate(datetime.date(datetime.date.today().year, 12, 31))
 	for d in balance_scheds:
-		pass
-
-
-
+		lb = frappe.new_doc("LB Entry")
+		lb.update({
+			"employee": d.employee,
+			"employee_name": d.employee_name,
+			"leave_type": d.leave_type,
+			"btype": "Add",
+			"bfrom": "Schedule Monthly",
+			"earned_date": now_date,
+			"valid_from": now_date,
+			"valid_to": year_end,
+			"credits": d.credits
+		})
+		lb.insert()
