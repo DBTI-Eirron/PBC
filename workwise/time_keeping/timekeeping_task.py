b021390e1e69cd4b1b85e8a687fe009caf8ec04e
Kinzunite <kinzunite@gmail.com>
2019-04-04 14:36:48 +0800

    Added LB Monthly scheduled tasks

--- workwise/time_keeping/timekeeping_task.py
+++ workwise/time_keeping/timekeeping_task.py
@@ -5,3 +5,14 @@ from frappe import _
 
 def get_scheduler():
 	pass
+
+def leave_balance_monthly():
+	balance_scheds = frappe.db.sql(""" SELECT LB.employee, LB.employee_name, LBS.leave_type, LBS.trigger_on, LBS.credit 
+		FROM `tabLeave Balance` LB 
+		INNER JOIN `tabLeave Balance Schedule` LBS ON LBS.`parent` = LB.`name` WHERE LBS.trigger_on = 'Every Month' """, as_dict=1)
+
+	for d in balance_scheds:
+		pass
+
+
+
