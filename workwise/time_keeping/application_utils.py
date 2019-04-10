39c5803d0e95dff209ffd4a84f45b0780353afab
administrator <administrator@erpextend.localdomain>
2019-04-10 05:31:36 +0000

    Added Admin Role on Rejection and Cancellation of applications

--- workwise/time_keeping/application_utils.py
+++ workwise/time_keeping/application_utils.py
@@ -128,7 +128,7 @@ def get_levelled_approval_rejection(self):
 	enable_employee_approvers = frappe.db.get_single_value('Timekeeping Settings', 'enable_employee_approvers')
 	if enable_employee_approvers > 0:
 		approver_level = frappe.db.sql(""" SELECT IFNULL(SUM(`level`), 0) as level FROM `tabEmployee Approvers` WHERE parenttype = "Employee" AND application = %s AND parent = %s AND approver_userid = %s """,(self.doctype, self.employee, frappe.session.user), as_dict=True)
-		if not "Administrator" in frappe.get_roles(frappe.session.user):
+		if not "Administrator" or not "Admin Approver" in frappe.get_roles(frappe.session.user):
 			if self.workflow_state == "Rejected":
 				if approver_level:
 					if int(approver_level[0].level) != int(self.last_approval_level+1):
