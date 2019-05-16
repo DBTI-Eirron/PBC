d8bce9bbc3483169a87deacdb3fc19426e5ca123
administrator <administrator@erpextend.localdomain>
2019-05-16 09:31:12 +0000

    Perfect Attendance Bugfix

--- workwise/time_keeping/report/perfect_attendance/perfect_attendance.py
+++ workwise/time_keeping/report/perfect_attendance/perfect_attendance.py
@@ -36,10 +36,34 @@ def get_columns(filters):
 	return columns
 
 def get_data(filters):
-	from_date = getdate(filters.from_date)
-	to_date = getdate(filters.to_date)
-	entries = frappe.db.sql("""SELECT name AS xname, biometrics_id, full_name, (SELECT COUNT(`name`) FROM `tabAttendance Register` WHERE work_hours <> work AND `employee`= xname AND target_date >= %s AND target_date <= %s) AS abs FROM `tabEmployee` WHERE `company` = %s""",(from_date,to_date,filters.company),as_dict=True)
-	data = format_entries(filters,entries)
+	cur_user = frappe.session.user
+	if not "Administrator" in frappe.get_roles(cur_user):
+		employees = frappe.db.sql(""" SELECT DISTINCT AR.employee as `xname`, TE.full_name, TE.biometrics_id, (SELECT COUNT(`name`) FROM `tabAttendance Register` WHERE work_hours <> work AND `employee`= xname AND is_restday <> 1 AND is_holiday <> 1 AND target_date >= %(from_date)s AND target_date <= %(to_date)s) AS abs
+				FROM `tabAttendance Register` AR INNER JOIN `tabEmployee` TE ON AR.employee = TE.`name` 
+				WHERE TE.company = %(company)s 
+				AND (AR.target_date BETWEEN %(from_date)s AND %(to_date)s)
+				AND TE.sensitivity IN (SELECT SL.`name` FROM `tabSensitivity Level` SL INNER JOIN `tabSensitivity Users` SU ON SU.parent = SL.`name` WHERE SU.allow_user = %(user)s) 
+				ORDER BY AR.employee
+				""",{ 
+				"company": filters.company,
+				"from_date": filters.from_date,
+				"to_date": filters.to_date,
+				"user": frappe.session.user
+			}, as_dict=True)
+	else:
+		employees = frappe.db.sql("""SELECT DISTINCT AR.employee as `xname`, TE.full_name, TE.biometrics_id, (SELECT COUNT(`name`) FROM `tabAttendance Register` WHERE work_hours <> work AND `employee`= xname AND is_restday <> 1 AND is_holiday <> 1 AND target_date >= %(from_date)s AND target_date <= %(to_date)s) AS abs
+				FROM `tabAttendance Register` AR INNER JOIN `tabEmployee` TE ON AR.employee = TE.`name` 
+				WHERE TE.company = %(company)s 
+				AND (AR.target_date BETWEEN %(from_date)s AND %(to_date)s)
+				AND TE.sensitivity IN (SELECT SL.`name` FROM `tabSensitivity Level` SL INNER JOIN `tabSensitivity Users` SU ON SU.parent = SL.`name`)
+				ORDER BY AR.employee
+				""",{ 
+				"company": filters.company,
+				"from_date": getdate(filters.from_date),
+				"to_date": getdate(filters.to_date)
+			}, as_dict=True)
+		
+	data = format_entries(filters,employees)
 	return data
 
 def format_entries(filters,entries):
