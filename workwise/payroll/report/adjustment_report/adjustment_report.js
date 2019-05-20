50e90f13a755fbf54a07070ff80aa96f9275d5d3
administrator <administrator@erpextend.localdomain>
2019-05-20 09:22:24 +0000

    Adjustment Report

---
+++ workwise/payroll/report/adjustment_report/adjustment_report.js
@@ -0,0 +1,49 @@
+// Copyright (c) 2016, HDI Systech and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Adjustment Report"] = {
+	"filters": [
+		{
+			"fieldname": "company",
+			"label": __("Company"),
+			"fieldtype": "Link",
+			"options": "Company",
+			"reqd": 1
+		},
+		{
+			"fieldname": "payroll_period",
+			"label": __("Period"),
+			"fieldtype": "Link",
+			"options": "Payroll Period",
+			"get_query": function() {
+				var company = frappe.query_report_filters_by_name.company.get_value();
+				return{
+					filters: {
+						'company': company
+					}
+				};
+			},
+			"reqd": 1
+		},
+		{
+			"fieldname": "employee",
+			"label": __("Employee"),
+			"fieldtype": "Link",
+			"options": "Employee",
+			"get_query": function() {
+				var company = frappe.query_report_filters_by_name.company.get_value();
+				return{
+					filters: {
+						'company': company
+					}
+				};
+			}
+		},
+		{
+			"fieldname": "hide_zero",
+			"label": __("Hide zero value"),
+			"fieldtype": "Check",
+		},
+	]
+}
---
+++ workwise/payroll/report/adjustment_report/adjustment_report.py
@@ -0,0 +1,213 @@
+# Copyright (c) 2013, HDI Systech and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+import frappe
+from frappe.utils import cint, flt, getdate, cstr
+from frappe import _
+
+def execute(filters=None):
+	columns, data = [], []
+	columns = get_columns(filters)
+	data = get_data(filters)
+
+	return columns, data
+
+def get_columns(employee_list):
+	columns = [
+		{
+			"fieldname": "employee",
+			"label": _("Employee"),
+			"fieldtype": "Link",
+			"options": "Employee",
+			"width": 140
+		},
+		{
+			"fieldname": "employee_name",
+			"label": _("Employee name"),
+			"fieldtype": "Data",
+			"width": 160
+		},
+		{
+			"fieldname": "income_absent",
+			"label": _("Income Absent"),
+			"fieldtype": "Data",
+			"width": 140
+		},
+		{
+			"fieldname": "deduction_absent",
+			"label": _("Deduction Absent"),
+			"fieldtype": "Data",
+			"width": 140
+		},
+		{
+			"fieldname": "income_uh",
+			"label": _("Income Unpaid Holiday"),
+			"fieldtype": "Data",
+			"width": 140
+		},
+		{
+			"fieldname": "deduction_uh",
+			"label": _("Deduction Unpaid Holiday"),
+			"fieldtype": "Data",
+			"width": 140
+		},
+		{
+			"fieldname": "income_ot",
+			"label": _("Income Overtime"),
+			"fieldtype": "Data",
+			"width": 140
+		},
+		{
+			"fieldname": "deduction_ot",
+			"label": _("Deduction Overtime"),
+			"fieldtype": "Data",
+			"width": 140
+		},
+		{
+			"fieldname": "income_nd",
+			"label": _("Income Nightdiff"),
+			"fieldtype": "Data",
+			"width": 140
+		},
+		{
+			"fieldname": "deduction_nd",
+			"label": _("Deduction Nightdiff"),
+			"fieldtype": "Data",
+			"width": 140
+		},
+		{
+			"fieldname": "income_late",
+			"label": _("Income Late"),
+			"fieldtype": "Data",
+			"width": 140
+		},
+		{
+			"fieldname": "deduction_late",
+			"label": _("Deduction Late"),
+			"fieldtype": "Data",
+			"width": 140
+		},
+		{
+			"fieldname": "income_ut",
+			"label": _("Income Undertime"),
+			"fieldtype": "Data",
+			"width": 140
+		},
+		{
+			"fieldname": "deduction_ut",
+			"label": _("Deduction Undertime"),
+			"fieldtype": "Data",
+			"width": 140
+		},
+	]
+
+	return columns
+
+def get_data(filters):
+	data = []
+	
+	register = frappe.db.sql("""SELECT * FROM `tabAdjustment Register` 
+		WHERE `company` = %(company)s AND `payroll_period` = %(payroll_period)s {conditions}
+		ORDER BY `employee_name` ASC""".format(conditions=get_conditions(filters)),{
+		"company": filters.company,
+		"employee": filters.employee,
+		"payroll_period": filters.payroll_period,
+	}, as_dict=True)
+
+	if register:
+		for reg in register:
+			income_absent = 0
+			deduction_absent = 0
+			income_uh = 0
+			deduction_uh = 0
+			income_ot = 0
+			deduction_ot = 0
+			income_nd = 0
+			deduction_nd = 0
+			income_late = 0
+			deduction_late = 0
+			income_ut = 0
+			deduction_ut = 0
+
+			if income_absent > 0:
+				income_absent = flt(reg.absent)
+			else:
+				deduction_absent = flt(reg.absent)
+
+			if income_uh > 0:
+				income_uh = flt(reg.unpaid_holiday)
+			else:
+				deduction_uh = flt(reg.unpaid_holiday)
+
+			if income_ot > 0:
+				income_ot = flt(reg.overtime)
+			else:
+				deduction_ot = flt(reg.overtime)
+
+			if income_nd > 0:
+				income_nd = flt(reg.nightdiff)
+			else:
+				income_nd = flt(reg.nightdiff)
+
+			if income_late > 0:
+				income_late = flt(reg.late)
+			else:
+				deduction_late = flt(reg.late)
+
+			if income_ut > 0:
+				income_ut = flt(reg.undertime)
+			else:
+				deduction_ut = flt(reg.undertime)
+
+			if filters.hide_zero == 1:
+				emp_total = income_absent + deduction_absent + income_uh + deduction_uh + income_ot + deduction_ot + income_nd + deduction_nd + income_late + deduction_late + income_ut + deduction_ut
+				if emp_total != 0:
+					row = {
+						"employee": reg.employee,
+						"employee_name": reg.employee_name,
+						"income_absent": '{:,.2f}'.format(income_absent),
+						"deduction_absent": '{:,.2f}'.format(deduction_absent),
+						"income_uh": '{:,.2f}'.format(income_uh),
+						"deduction_uh": '{:,.2f}'.format(deduction_uh),
+						"income_ot": '{:,.2f}'.format(income_ot),
+						"deduction_ot": '{:,.2f}'.format(deduction_ot),
+						"income_nd": '{:,.2f}'.format(income_nd),
+						"deduction_nd": '{:,.2f}'.format(deduction_nd),
+						"income_late": '{:,.2f}'.format(income_late),
+						"deduction_late": '{:,.2f}'.format(deduction_late),
+						"income_ut": '{:,.2f}'.format(income_ut),
+						"deduction_ut": '{:,.2f}'.format(deduction_ut),
+					}
+					data.append(row)
+			else:
+				row = {
+					"employee": reg.employee,
+					"employee_name": reg.employee_name,
+					"income_absent": '{:,.2f}'.format(income_absent),
+					"deduction_absent": '{:,.2f}'.format(deduction_absent),
+					"income_uh": '{:,.2f}'.format(income_uh),
+					"deduction_uh": '{:,.2f}'.format(deduction_uh),
+					"income_ot": '{:,.2f}'.format(income_ot),
+					"deduction_ot": '{:,.2f}'.format(deduction_ot),
+					"income_nd": '{:,.2f}'.format(income_nd),
+					"deduction_nd": '{:,.2f}'.format(deduction_nd),
+					"income_late": '{:,.2f}'.format(income_late),
+					"deduction_late": '{:,.2f}'.format(deduction_late),
+					"income_ut": '{:,.2f}'.format(income_ut),
+					"deduction_ut": '{:,.2f}'.format(deduction_ut),
+				}
+				data.append(row)
+	return data
+
+def get_conditions(filters):
+	conditions = []
+	if filters.employee:
+		conditions.append("`employee` = %(employee)s")
+
+	return "and {}".format(" and ".join(conditions)) if conditions else "" 
+
+
+
+
+
