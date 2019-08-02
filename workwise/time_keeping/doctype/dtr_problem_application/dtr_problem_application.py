8b033fdfe746af24150da2894455f5bfc9759f76
k <kinzunite@gmail.com>
2019-08-02 16:24:04 +0800

    Bugfix for DTRP email feature

--- workwise/time_keeping/doctype/dtr_problem_application/dtr_problem_application.py
+++ workwise/time_keeping/doctype/dtr_problem_application/dtr_problem_application.py
@@ -9,7 +9,7 @@ from frappe import _
 from frappe.utils import nowdate, cstr, getdate
 from frappe.model.document import Document
 from workwise.time_keeping.application_utils import ( grant_head_subordinate_access, get_approver_and_date, validate_approve_own_application, validate_reject_cancel_own_application, 
-change_owner, get_levelled_approval, get_levelled_approval_rejection, clear_approval_history, validate_inactive_employee)
+change_owner, get_levelled_approval, get_levelled_approval_rejection, clear_approval_history, validate_inactive_employee, get_approver_email_list, get_cancelled_by_and_date )
 
 class DTRProblemApplication(Document):
 	def validate(self):
