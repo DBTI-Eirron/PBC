fcdb2cd3f3062aac9350af9da952143e0a43bf23
administrator <administrator@erpextend.localdomain>
2019-08-14 07:32:18 +0000

    workwise/time_keeping/report/employee_schedule/employee_schedule.py

--- workwise/time_keeping/report/employee_schedule/employee_schedule.py
+++ workwise/time_keeping/report/employee_schedule/employee_schedule.py
@@ -77,7 +77,8 @@ def get_data(filters):
 
 	pay_from, pay_to = frappe.db.get_value("Payroll Period", filters.payroll_period, ["attendance_from", "attendance_to"])
 	schedule = get_schedule(filters.employee, pay_from, pay_to)
-	for sched in schedule: 
+	for sched in schedule:
+		pre_shift, post_shift = frappe.db.get_value("Work Shift", sched['work_shift'], ["setup_preshift", "end_postshift"]) 
 		entry = {
 			"work_shift": sched['work_shift'],
 			"time_in": sched['datetime_in'],
