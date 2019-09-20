9f54e002c44645e5110c2e5bbd13bae0842a071a
administrator <administrator@erpextend.localdomain>
2019-09-20 02:46:11 +0000

    Bugfix for Loan Application

--- workwise/payroll/doctype/loan_application/loan_application_list.js
+++ workwise/payroll/doctype/loan_application/loan_application_list.js
@@ -3,11 +3,11 @@ frappe.listview_settings['Loan Application'] = {
 	get_indicator: function(doc) {
 		if (cint(doc.on_hold) == 1)  {
 			return [__("On Hold"), "red"]
-		} else if (flt(doc.paid_amount) < 1 && cint(doc.on_hold) != 1)  {
+		} else if (flt(doc.paid_amount) < 1 && flt(doc.unpaid_amount) > 0 && cint(doc.on_hold) != 1)  {
 			return [__("Entered"), "blue"]
-		} else if (flt(doc.paid_amount) > 0 && cint(doc.on_hold != 1) )  {
+		} else if (flt(doc.paid_amount) > 0 && flt(doc.unpaid_amount) > 0 && cint(doc.on_hold != 1) )  {
 			return [__("Active"), "orange"]
-		} else if (flt(doc.paid_amount) == flt(doc.unpaid_amount) && cint(doc.on_hold != 1) )  {
+		} else if (flt(doc.unpaid_amount) == 0 && cint(doc.on_hold != 1) )  {
 			return [__("Fully Paid"), "green"]
 		} 
 	}
