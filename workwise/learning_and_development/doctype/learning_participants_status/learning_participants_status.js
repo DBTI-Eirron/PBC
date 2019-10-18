dfd41f90dfc9f71f5737669a99489a10f873d3ca
k <kinzunite@gmail.com>
2019-10-18 11:00:52 +0800

    Learning and Development Update

--- workwise/learning_and_development/doctype/learning_participants_status/learning_participants_status.js
+++ workwise/learning_and_development/doctype/learning_participants_status/learning_participants_status.js
@@ -3,6 +3,27 @@
 
 frappe.ui.form.on('Learning Participants Status', {
 	refresh: function(frm) {
+		frappe.call({
+			method: "get_program_session",
+			doc: frm.doc,
+			callback: function(r) {
+				frm.set_df_property('program_session', 'options', r.message);
+				frm.refresh_field('program_session');
+			}
+		});
+	},
 
-	}
+	program_session: function(frm) {
+		frappe.call({
+			method: "get_attendance_data",
+			doc: frm.doc,
+			callback: function(r) {
+				frm.refresh_field('participants');
+			}
+		});
+	},
+
+	on_submit: function(frm) {
+		frappe.set_route('Form', 'Learning Event', frm.doc.learning_event);
+	},
 });
