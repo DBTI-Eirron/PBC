4a38e775ed35cf247619faf8446e6a9793e234fc
k <kinzunite@gmail.com>
2019-10-30 17:46:25 +0800

    Attendance Summarry Processed Float Control

--- workwise/time_keeping/report/attendance_summary_processed/attendance_summary_processed.py
+++ workwise/time_keeping/report/attendance_summary_processed/attendance_summary_processed.py
@@ -126,6 +126,12 @@ def get_columns(filters):
 		},
 	]
 
+	if filters.flt_precision:
+		precision_fields = ["work","break","late","overtime","overtime_ex","overtime_nd","nightdiff","cto","undertime"]
+		for d in columns:
+			if d.get('fieldname') in precision_fields:
+				d['precision'] = cint(filters.flt_precision)
+
 	return columns
 
 def get_result(filters):
