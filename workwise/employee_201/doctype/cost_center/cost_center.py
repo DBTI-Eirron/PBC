cc4c462322341e06caa124454e72e6876a0ef876
k <kinzunite@gmail.com>
2020-01-20 15:14:46 +0800

    Cost Center Company

--- workwise/employee_201/doctype/cost_center/cost_center.py
+++ workwise/employee_201/doctype/cost_center/cost_center.py
@@ -30,7 +30,7 @@ class CostCenter(NestedSet):
 				frappe.throw("Parent Cost Center is Required if not group")
 
 	def update_company(self):
-		cc_dict = frappe.db.sql("""SELECT `name`, lft, rgt, parent FROM `tabCost Center` WHERE parent = 'Cost Center Structure'""",as_dict=True)
+		cc_dict = frappe.db.sql("""SELECT `name`, lft, rgt, parent FROM `tabCost Center` WHERE parent_cost_center = 'Cost Center Structure'""",as_dict=True)
 		for cc in cc_dict:
 			if cc.lft <= self.lft and cc.rgt >= self.rgt:
 				self.company = cc.name
