c09bb1ebfa9e78f9a4a413b237fe63149f4ca336
k <kinzunite@gmail.com>
2020-03-02 11:15:40 +0800

    Get schedule imported in time keeping patches

--- workwise/time_keeping/timekeeping_patches.py
+++ workwise/time_keeping/timekeeping_patches.py
@@ -1,6 +1,7 @@
 from __future__ import unicode_literals
 import frappe, datetime
 from frappe.utils import cint, flt, nowdate, add_days, getdate, fmt_money, add_to_date, cstr
+from workwise.time_keeping.attendance_utils import get_schedule
 from frappe import _
 
 #APPLICATION PATCHES
