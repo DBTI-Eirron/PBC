1194968493b251707b2d6c13325e941565c5fcf0
k <kinzunite@gmail.com>
2020-04-01 14:56:36 +0800

    Certificate of maternity EOC

--- workwise/employee_201/doctype/certificate_of_maternity/certificate_of_maternity.py
+++ workwise/employee_201/doctype/certificate_of_maternity/certificate_of_maternity.py
@@ -11,8 +11,15 @@ class CertificateofMaternity(Document):
 	def validate(self):
 		pass
 	def get_to_date(self):
-		resign_date = frappe.get_value('Employee', self.employee, 'date_resigned')
+		resign_date,eoc,retired_date,terminated_date = frappe.get_value('Employee', self.employee, ['date_resigned','date_contract_ended','date_retired','date_terminated'])
 		if resign_date:
 			self.to_date = resign_date
+		elif eoc:
+			self.to_date = eoc
+		elif retired_date:
+			self.to_date = retired_date
+		elif terminated_date:
+			self.to_date = terminated_date
 		else:
 			self.to_date = date.today()
+			self.to_present = 1
