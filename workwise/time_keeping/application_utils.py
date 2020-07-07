fb0d6981462b3aa5c4cd50956e4327e06d2dcbdd
k <kinzunite@gmail.com>
2020-07-07 14:56:55 +0800

    Bugfix for Recurring Entry

--- workwise/time_keeping/application_utils.py
+++ workwise/time_keeping/application_utils.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 import frappe, datetime
 from datetime import time, datetime, timedelta
-from frappe.utils import cstr, cint, flt, nowdate, add_days, getdate, fmt_money, now_datetime, add_to_date
+from frappe.utils import cstr, cint, flt, nowdate, add_days, getdate, fmt_money, now_datetime, add_to_date, now
 from frappe import _, msgprint
 from workwise.time_keeping.attendance_utils import (get_timecard_list, get_card_within, get_sorted_card, get_all_dtrp, get_schedule)
 
@@ -136,7 +136,7 @@ def validate_cutoff_approval_date(self):
 			target_date = self.from_date
 
 		if self.doctype in ['DTR Problem Application']:
-			target_date = self.from_date
+			target_date = self.target_date
 
 		if target_date:
 			approvals_cutoff = frappe.db.sql("""SELECT MAX(`approval_cutoff`) as `approval_cutoff` FROM `tabPayroll Period` WHERE `company` = %s AND %s BETWEEN `attendance_from` AND `attendance_to` """,(self.company, target_date), as_dict=1)
@@ -185,7 +185,7 @@ def set_levelled_approval_to_progress(self, approver_level):
 	self.db_set("last_approval_level", approver_level[0].level)
 	self.db_set("workflow_state", "Approval in Progress")
 	self.db_set("approved_by", frappe.session.user)
-	self.db_set("approved_on", nowdate())
+	self.db_set("approved_on", now())
 	approver_name = frappe.db.sql("""SELECT full_name FROM `tabEmployee` WHERE `user_id` = %s LIMIT 1""",( frappe.session.user ), as_dict=1)
 	if approver_name:
 		self.db_set("approver_name", cstr(approver_name[0].full_name))
@@ -204,7 +204,7 @@ def set_levelled_approval_to_approved(self, highest_level):
 	self.db_set("last_approval_level", highest_level[0].level)
 	self.db_set("workflow_state", "Approved")
 	self.db_set("approved_by", frappe.session.user)
-	self.db_set("approved_on", nowdate())
+	self.db_set("approved_on", now())
 	approver_name = frappe.db.sql("""SELECT full_name FROM `tabEmployee` WHERE `user_id` = %s LIMIT 1""",( frappe.session.user ), as_dict=1)
 	if approver_name:
 		self.db_set("approver_name", cstr(approver_name[0].full_name))
@@ -227,7 +227,7 @@ def get_levelled_approval_rejection(self):
 def get_approver_and_date(self):
 	if self.workflow_state == "Approved":
 		self.db_set("approved_by", frappe.session.user)
-		self.db_set("approved_on", nowdate())
+		self.db_set("approved_on", now())
 		approver_name = frappe.db.sql("""SELECT full_name FROM `tabEmployee` WHERE `user_id` = %s LIMIT 1""",( frappe.session.user ), as_dict=1)
 		if approver_name:
 			self.db_set("approver_name", cstr(approver_name[0].full_name))
