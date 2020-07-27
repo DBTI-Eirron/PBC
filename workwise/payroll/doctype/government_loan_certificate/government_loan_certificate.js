c1e262f86794f14446ce759e496b6306f5cc146d
k <kinzunite@gmail.com>
2020-07-27 14:39:50 +0800

    Biometrics Upload Enhanced and change text color

---
+++ workwise/payroll/doctype/government_loan_certificate/government_loan_certificate.js
@@ -0,0 +1,58 @@
+// Copyright (c) 2019, HDI Systech and contributors
+// For license information, please see license.txt
+this.frm.get_field("employees").grid.cannot_add_rows = true;
+cur_frm.add_fetch('certified_by', 'full_name', 'certified_name');
+frappe.ui.form.on('Government Loan Certificate', {
+	refresh: function(frm) {
+		if(frm.doc.docstatus == 1){
+			frappe.call({
+				method: "workwise.setup.doctype.jasper_form.jasper_form.get_forms",
+				args:{
+					doctype_name: "Government Loan Certificate"
+				},
+				callback: function(r) {
+					r.message.forEach(function(item) {
+						frm.add_custom_button(__(item.form_label),
+						function() {
+							window.open("http://"+ item.form_ip +":"+ item.form_port +"/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2F"+ item.form_folder +"&reportUnit=%2FReports%2F"+ item.form_name +"&standAlone=true&j_username=jasperadmin&j_password=jasperadmin&output=pdf&filter1="+frm.doc.name+"");
+						});
+					});
+				}
+			});
+		}
+
+		cur_frm.set_query("employee", function() {
+			return {
+				"filters": {
+					"is_active": 1,
+				}
+			};
+		});
+
+		cur_frm.set_query("certified_by", function() {
+			return {
+				"filters": {
+					"is_active": 1,
+				}
+			};
+		});
+	},
+
+	add: function(frm) {
+		if(frm.doc.type && frm.doc.certified_by && frm.doc.sensitivity_level && frm.doc.from_date && frm.doc.to_date){
+			frappe.call({
+				method: "get_employees",
+				doc: frm.doc,
+				callback: function(r) {
+					frm.refresh_fields();
+				}
+			});
+		}
+	},
+
+	clear: function(frm) {
+		frm.doc.employees = null
+		frm.refresh_fields()
+	},
+
+});
