aef6db64b9bcf32233da10aaa339f8805e2168ce
k <kinzunite@gmail.com>
2020-07-31 22:22:28 +0800

    Additional field and Validation for Sensitivity Level added in Loan Application and Loan Restructure

--- workwise/payroll/doctype/loan_restructure/loan_restructure.js
+++ workwise/payroll/doctype/loan_restructure/loan_restructure.js
@@ -1,8 +1,14 @@
 // Copyright (c) 2019, HDI Systech and contributors
 // For license information, please see license.txt
 
+cur_frm.add_fetch('employee','sensitivity','sensitivity');
+
 frappe.ui.form.on('Loan Restructure', {
 	refresh: function(frm) {
 
-	}
+	},
+
+	setup: function(frm) {
+		frm.add_fetch('employee','sensitivity','sensitivity');
+	},
 });
