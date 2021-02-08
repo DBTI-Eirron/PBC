c7111b605ca1efb2fd42e908f1f8f62cdd881cde
k <kinzunite@gmail.com>
2021-02-08 17:59:31 +0800

    Loan Status Report Bug fix> on Loan paid amount Within dates

--- workwise/payroll/report/loan_status/loan_status.py
+++ workwise/payroll/report/loan_status/loan_status.py
@@ -164,8 +164,8 @@ def get_data(filters):
 	loans = get_loans(filters)
 
 	for l in loans:
-		total_paid = get_loan_amount(filters, l)
-		l['total_paid'] = flt(total_paid)
+		total_loans = get_loan_amount(filters, l)
+		l['total_paid'] = flt(total_loans)
 
 	for loan in loans: 
 		data.append(loan)
