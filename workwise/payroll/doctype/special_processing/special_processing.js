de641d0c3f3d194be0e3589c6b8750fcc8b73624
Allen <allen.victorino@opensoftsolutions.com>
2021-05-26 18:10:21 +0800

    Special Processing - Auto fill Period Group

--- workwise/payroll/doctype/special_processing/special_processing.js
+++ workwise/payroll/doctype/special_processing/special_processing.js
@@ -19,6 +19,7 @@ frappe.ui.form.on('Special Processing', {
 		frm.add_fetch("period", "payroll_date", "payroll_date");	
 		frm.add_fetch("period", "schedule", "schedule");
 		frm.add_fetch("period", "payroll_year", "payroll_year");
+		frm.add_fetch("period", "period_group", "period_group");
 	},
 
 	refresh: function(frm) {
