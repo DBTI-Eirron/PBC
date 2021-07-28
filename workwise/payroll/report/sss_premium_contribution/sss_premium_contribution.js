d21fcf287a65aafa125b94f65186164aabc7e654
JmOSI <39688352+JmOSI@users.noreply.github.com>
2021-07-28 17:04:09 +0800

    Revert "SSS certificate moved to government certificate"
    This reverts commit 8c31df2b1f7a46f611a44f392546e9f3191c73e6.

--- workwise/payroll/report/sss_premium_contribution/sss_premium_contribution.js
+++ workwise/payroll/report/sss_premium_contribution/sss_premium_contribution.js
@@ -58,6 +58,14 @@ frappe.query_reports["SSS Premium Contribution"] = {
 					var password = r.message.jasper_pass;
 					var session_user = r.message.session_user;
 
+					report.page.add_inner_button(__("Print SSS Certificate"), function() {
+						var from_date = frappe.query_report_filters_by_name.from_date.get_value();
+						var to_date = frappe.query_report_filters_by_name.to_date.get_value();
+						var company = frappe.query_report_filters_by_name.company.get_value();
+						var group = frappe.query_report_filters_by_name.period_group.get_value();
+						window.open("http://"+host_link+"/jasperserver/flow.html?_flowId=viewReportFlow&_flowId=viewReportFlow&ParentFolderUri=%2FReports&reportUnit=%2FReports%2Fsss_certificate&standAlone=true&j_username="+username+"&j_password="+password+"&output=pdf&company="+company+"&user="+session_user+"&group="+group+"&from_date="+from_date+"&to_date="+to_date);
+					});
+
 					report.page.add_inner_button(__("Print SSS Premium Contribution"), function() {
 						var from_date = frappe.query_report_filters_by_name.from_date.get_value();
 						var to_date = frappe.query_report_filters_by_name.to_date.get_value();
