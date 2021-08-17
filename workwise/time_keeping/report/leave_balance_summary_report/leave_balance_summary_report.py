828c36f7369ac794ffa3eea897cbaf052517b1ed
Chopeleng <cid.zaragoza@opensoftsolutions.com>
2021-08-17 17:31:45 +0800

    Error fix for enye character in leave balance summary report

--- workwise/time_keeping/report/leave_balance_summary_report/leave_balance_summary_report.py
+++ workwise/time_keeping/report/leave_balance_summary_report/leave_balance_summary_report.py
@@ -156,9 +156,9 @@ def get_data(filters):
 		data.append({})
 	
 	for per in sorted(data_entry.keys()):
-		data += [{"employee":"<b>"+str(per)+"</b>"}]
+		data += [{"employee":"<b>"+cstr(per)+"</b>"}]
 		for loc in sorted(data_entry[per].keys()):
-			data += [{"employee":"<b>"+str(loc)+"</b>"}]
+			data += [{"employee":"<b>"+cstr(loc)+"</b>"}]
 			for ent in data_entry[per][loc]:
 				if ent:
 					data += ent
