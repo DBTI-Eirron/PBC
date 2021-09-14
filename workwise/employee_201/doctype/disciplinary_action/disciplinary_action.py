92483d4bd4f17d0837296ae275b537e23f719ec9
Allen <allen.victorino@opensoftsolutions.com>
2021-09-14 10:14:38 +0800

    Disciplinary Action - suspension days exclude rest day or holiday added

--- workwise/employee_201/doctype/disciplinary_action/disciplinary_action.py
+++ workwise/employee_201/doctype/disciplinary_action/disciplinary_action.py
@@ -3,11 +3,14 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+import frappe, datetime
 from frappe.model.document import Document
 from frappe.model.mapper import get_mapped_doc
+from frappe.utils import cint, flt, getdate, cstr, add_to_date, get_datetime, nowdate
 from frappe import throw,_
+from workwise.time_keeping.timekeeping_utils import add_date, db_datetime_str
 from workwise.time_keeping.timekeeping_utils import chk_time_format, timediff_hrs, timediff_mins, datediff_days
+from workwise.time_keeping.attendance_utils import get_actual_logs
 
 class DisciplinaryAction(Document):
 	def validate(self):
@@ -27,6 +30,25 @@ class DisciplinaryAction(Document):
 
 	def calc_days(self):
 		self.suspension = int( (datediff_days(self.suspended_from, self.suspended_to, "%Y-%m-%d")).days + 1 )
+		if self.ex_holiday or self.ex_restday:
+			shift = get_actual_logs(self.employee, getdate(self.suspended_from), getdate(self.suspended_to))
+			company = frappe.db.get_value("Employee", self.employee, ["company"] )
+
+			holidays = frappe.db.sql("""SELECT holiday_name, holiday_date, is_special, location FROM `tabHoliday` 
+				WHERE company = %s AND holiday_date >= %s AND holiday_date <= %s
+				ORDER BY holiday_date ASC""",(company, getdate(self.suspended_from), getdate(self.suspended_to)), as_dict=True)
+
+
+			for s in shift:
+				if (s['is_restday'] and self.ex_restday):
+					self.suspension = (int(self.suspension) - 1)
+					continue
+				if self.ex_holiday:
+					for h in holidays:
+						if s['target_date'] == h['holiday_date']:
+							self.suspension = (int(self.suspension) - 1)
+							continue
+
 
 @frappe.whitelist()
 def make_movement(source_name, target_doc=None):
