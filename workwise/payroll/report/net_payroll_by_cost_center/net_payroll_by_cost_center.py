0b165d18774af22de726ecc67e8cc2c8d6f8261d
AaronOSI <aaron.alcarde@opensoftsolutions.com>
2023-05-03 11:11:03 +0800

    Net Payroll by Cost Center - Fix mapping of query

--- workwise/payroll/report/net_payroll_by_cost_center/net_payroll_by_cost_center.py
+++ workwise/payroll/report/net_payroll_by_cost_center/net_payroll_by_cost_center.py
@@ -58,7 +58,7 @@ def get_data(filters):
 		register = frappe.db.sql(""" SELECT PR.employee, PR.employee_name, PR.net_payroll, E.cost_center
 			FROM `tabPayroll Register` PR 
 			INNER JOIN tabEmployee E ON E.`name` = PR.employee WHERE 
-			E.sensitivity IN (SELECT SL.`name` FROM `tabSensitivity Level` SL INNER JOIN `tabSensitivity Users` SU ON SU.parent = SL.`name` WHERE SU.allow_user = %(user)s)
+			E.sensitivity IN (SELECT SL.`name` FROM `tabSensitivity Level` SL INNER JOIN `tabSensitivity Users` SU ON SU.parent = SL.`name` WHERE SU.allow_user = %s)
 			AND E.cost_center = %s AND PR.period = %s """, (frappe.session.user, filters.cost_center, filters.period), as_dict=1)
 	else:
 		register = frappe.db.sql(""" SELECT PR.employee, PR.employee_name, PR.net_payroll, E.cost_center
