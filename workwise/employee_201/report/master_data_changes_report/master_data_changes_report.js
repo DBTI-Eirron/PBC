b554f196b91496320ceddf120ae0688a4d22905b
k <kinzunite@gmail.com>
2025-06-11 14:50:33 +0800

    Master Data Report

---
+++ workwise/employee_201/report/master_data_changes_report/master_data_changes_report.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, OSI and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Master Data Changes Report"] = {
+	"filters": [
+
+	]
+}
