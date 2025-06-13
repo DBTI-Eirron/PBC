4d1449a410d57d71ac48e5da8dc660e84ba7faa5
Aaron Labini <labini_a@direcbti.int>
2025-06-13 13:50:23 +0800

    Filter and Backend Update

--- workwise/employee_201/report/employee_master_data_changes/employee_master_data_changes.js
+++ workwise/employee_201/report/employee_master_data_changes/employee_master_data_changes.js
@@ -31,18 +31,78 @@ frappe.query_reports["Employee Master Data Changes"] = {
 			fieldtype: "Select",
 			options: "\nJob Rotation\nRetirement\nResignation\nRegularization\nRehire\nTransfer\nTermination\nSalary Adjustment\nExtension of Services\nEnd of Contract\nPromotion\nChange of Name\nAdd Bank Details\nChange Bank Details"
 		}
+	],
 
-		// {
-		// 	fieldname: "from_date",
-		// 	label: __("From Date"),
-		// 	fieldtype: "Date",
-		// 	"reqd": 1
-		// },
-		// {
-		// 	fieldname: "to_date",
-		// 	label: __("To Date"),
-		// 	fieldtype: "Date",
-		// 	"reqd": 1
-		// }
-	]
-}
+	// Use refresh event to apply styling after report loads
+	refresh: function(report) {
+		// Wait for the report to fully render
+		setTimeout(function() {
+			// Remove borders from rows containing bold text (headers)
+			$('.report-wrapper table tbody tr').each(function() {
+				var $row = $(this);
+				var firstCell = $row.find('td:first-child');
+				
+				// Check if first cell contains bold text (header row)
+				if (firstCell.find('b').length > 0 || firstCell.html().indexOf('<b>') !== -1) {
+					$row.find('td').css({
+						'border': 'none',
+						'border-top': 'none',
+						'border-bottom': 'none',
+						'border-left': 'none',
+						'border-right': 'none'
+					});
+				}
+				
+				// Check if all cells in row are empty (spacer row)
+				var isEmpty = true;
+				$row.find('td').each(function() {
+					if ($(this).text().trim() !== '') {
+						isEmpty = false;
+						return false;
+					}
+				});
+				
+				if (isEmpty) {
+					$row.find('td').css({
+						'border': 'none',
+						'border-top': 'none',
+						'border-bottom': 'none',
+						'border-left': 'none',
+						'border-right': 'none',
+						'height': '25px'
+					});
+				}
+
+				// Style signature-related rows
+				var lastThreeCells = $row.find('td').slice(-3); // Get last 3 cells (prepared_by, checked_by, approved_by)
+				
+				// Check if this is a signature line row (contains underscores)
+				if (lastThreeCells.filter(function() { 
+					return $(this).text().indexOf('________________________') !== -1; 
+				}).length > 0) {
+					$row.find('td').css({
+						'border': 'none',
+						'text-align': 'center',
+						'font-weight': 'normal',
+						'padding-top': '20px'
+					});
+				}
+				
+				// Check if this is a signatory label row (contains "Prepared By", "Checked By", "Approved By")
+				if (lastThreeCells.filter(function() { 
+					var text = $(this).html();
+					return text.indexOf('Prepared By') !== -1 || text.indexOf('Checked By') !== -1 || text.indexOf('Approved By') !== -1;
+				}).length > 0) {
+					$row.find('td').css({
+						'border': 'none',
+						'text-align': 'center',
+						'font-weight': 'bold',
+						'padding-top': '5px'
+					});
+				}
+				
+
+			});
+		}, 500); // Wait 500ms for report to render
+	}
+};
--- workwise/employee_201/report/employee_master_data_changes/employee_master_data_changes.py
+++ workwise/employee_201/report/employee_master_data_changes/employee_master_data_changes.py
@@ -219,7 +219,8 @@ def get_data(filters):
 				"prepared_by_name": "",
 				"checked_by_name": "",
 				"approved_by_name": "",
-				"is_header": True  # Flag to identify header rows
+				"is_header": True,
+				"_style": "border: none !important; background-color: transparent;" 
 			}
 			data.append(header_entry)
 			
@@ -284,7 +285,8 @@ def get_data(filters):
 					"prepared_by_name": created_by,
 					"checked_by_name": checked_by,
 					"approved_by_name": approved_by,
-					"is_header": False  # Flag to identify data rows
+					"is_header": False,
+					"_style": "border: none !important; background-color: transparent;" 
 				}
 
 				data.append(entry)
@@ -309,6 +311,76 @@ def get_data(filters):
 				"is_spacer": True  # Flag to identify spacer rows
 			}
 			data.append(spacer_entry)
+
+		    	# Add signatory section at the bottom of the report
+			if data:  # Only add signatories if there's data
+				# Add extra spacing before signatories
+				for i in range(3):  # Add 3 empty rows for spacing
+					spacing_entry = {
+						"movement_type": "",
+						"employee": "",
+						"employee_name": "",
+						"effective_on": "",
+						"new_employee_name": "",
+						"transfer_type": "",
+						"current_rate": "",
+						"new_rate": "",
+						"current_position": "",
+						"new_position": "",
+						"current_bank_details": "",
+						"new_bank_details": "",
+						"prepared_by_name": "",
+						"checked_by_name": "",
+						"approved_by_name": "",
+						"is_signature_spacing": True,
+						"_style": "border: none !important; background-color: transparent; height: 25px;"
+					}
+					data.append(spacing_entry)
+
+				# Add signature lines
+				signature_line_entry = {
+					"movement_type": "",
+					"employee": "",
+					"employee_name": "",
+					"effective_on": "",
+					"new_employee_name": "",
+					"transfer_type": "",
+					"current_rate": "",
+					"new_rate": "",
+					"current_position": "",
+					"new_position": "",
+					"current_bank_details": "",
+					"new_bank_details": "",
+					"prepared_by_name": "________________________",
+					"checked_by_name": "________________________",
+					"approved_by_name": "________________________",
+					"is_signature_line": True,
+					"_style": "border: none !important; background-color: transparent; text-align: center;"
+				}
+				data.append(signature_line_entry)
+
+				# Add signatory labels
+				signatory_labels_entry = {
+					"movement_type": "",
+					"employee": "",
+					"employee_name": "",
+					"effective_on": "",
+					"new_employee_name": "",
+					"transfer_type": "",
+					"current_rate": "",
+					"new_rate": "",
+					"current_position": "",
+					"new_position": "",
+					"current_bank_details": "",
+					"new_bank_details": "",
+					"prepared_by_name": "<b>Prepared By</b>",
+					"checked_by_name": "<b>Checked By</b>",
+					"approved_by_name": "<b>Approved By</b>",
+					"is_signatory_label": True,
+					"_style": "border: none !important; background-color: transparent; text-align: center; font-weight: bold;"
+				}
+				data.append(signatory_labels_entry)
+
 
 	except Exception as e:
 		frappe.msgprint("Error in SQL query: {0}".format(str(e)))
